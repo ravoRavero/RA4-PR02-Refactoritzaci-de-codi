f49737948014cf237d5c51c2c54db5977fd401ce
Rahim <muhammadrahim.naveed.25@ibadia.cat>
2026-05-21 22:51:42 +0200

    Apply Extract Variable to improve readability

--- src/Magatzem.java
+++ src/Magatzem.java
@@ -13,73 +13,84 @@ class Magatzem {
     }
 
     public void actualitzarEstat() {
+
         for (int i = 0; i < articles.length; i++) {
 
-            if (!articles[i].nom.equals("Formatge Gidurat")
-                    && !articles[i].nom.equals("Entrades per al Concert del Trobador")) {
+            Article article = articles[i];
 
-                if (articles[i].qualitat > QUALITAT_MINIMA) {
+            boolean esFormatge =
+                    article.nom.equals("Formatge Gidurat");
 
-                    if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                        articles[i].qualitat = articles[i].qualitat - 1;
+            boolean esEntrada =
+                    article.nom.equals("Entrades per al Concert del Trobador");
+
+            boolean esLegendari =
+                    article.nom.equals("Martell de Thor (Llegendari)");
+
+            if (!esFormatge && !esEntrada) {
+
+                if (article.qualitat > QUALITAT_MINIMA) {
+
+                    if (!esLegendari) {
+                        article.qualitat = article.qualitat - 1;
                     }
                 }
 
             } else {
 
-                if (articles[i].qualitat < MAX_QUALITAT) {
+                if (article.qualitat < MAX_QUALITAT) {
 
-                    articles[i].qualitat = articles[i].qualitat + 1;
+                    article.qualitat = article.qualitat + 1;
 
-                    if (articles[i].nom.equals("Entrades per al Concert del Trobador")) {
+                    if (esEntrada) {
 
-                        if (articles[i].diesPerVendre <= LIMIT_ENTRADES_10_DIES) {
+                        if (article.diesPerVendre <= LIMIT_ENTRADES_10_DIES) {
 
-                            if (articles[i].qualitat < MAX_QUALITAT) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
+                            if (article.qualitat < MAX_QUALITAT) {
+                                article.qualitat = article.qualitat + 1;
                             }
                         }
 
-                        if (articles[i].diesPerVendre <= LIMIT_ENTRADES_5_DIES) {
+                        if (article.diesPerVendre <= LIMIT_ENTRADES_5_DIES) {
 
-                            if (articles[i].qualitat < MAX_QUALITAT) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
+                            if (article.qualitat < MAX_QUALITAT) {
+                                article.qualitat = article.qualitat + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                articles[i].diesPerVendre = articles[i].diesPerVendre - 1;
+            if (!esLegendari) {
+                article.diesPerVendre = article.diesPerVendre - 1;
             }
 
-            if (articles[i].diesPerVendre < 0) {
+            if (article.diesPerVendre < 0) {
 
-                if (!articles[i].nom.equals("Formatge Gidurat")) {
+                if (!esFormatge) {
 
-                    if (!articles[i].nom.equals("Entrades per al Concert del Trobador")) {
+                    if (!esEntrada) {
 
-                        if (articles[i].qualitat > QUALITAT_MINIMA) {
+                        if (article.qualitat > QUALITAT_MINIMA) {
 
-                            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                                articles[i].qualitat = articles[i].qualitat - 1;
+                            if (!esLegendari) {
+                                article.qualitat = article.qualitat - 1;
                             }
                         }
 
                     } else {
 
-                        articles[i].qualitat =
-                                articles[i].qualitat - articles[i].qualitat;
+                        article.qualitat =
+                                article.qualitat - article.qualitat;
                     }
 
                 } else {
 
-                    if (articles[i].qualitat < MAX_QUALITAT) {
-                        articles[i].qualitat = articles[i].qualitat + 1;
+                    if (article.qualitat < MAX_QUALITAT) {
+                        article.qualitat = article.qualitat + 1;
                     }
                 }
             }
         }
     }
-}g
+}
